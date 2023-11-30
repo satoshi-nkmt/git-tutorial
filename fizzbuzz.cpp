59beb0b2349de30d6ea08bda4ef068d6a3211865
HosodaRyo <j2031@matsue-ct.ac.jp>
2023-11-30 15:58:55 +0900

    second commit

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -9,3 +9,13 @@ int main() {
   for (int i = 1; i <= COUNT; ++i) {
     if (i % 3 == 0)
       cout << "Fizz, ";
+   else if (i % 5 == 0)
+      cout << "Buzz, ";
+    else if (i % 15 == 0)
+      cout << "FizzBuzz, ";
+    else
+      cout << i << ", ";
+  }
+
+  return EXIT_SUCCESS;
+}
