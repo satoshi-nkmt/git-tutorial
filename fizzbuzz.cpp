ba6d48bf0a9f2ba9c4e86c775560207d42fffd97
HosodaRyo <j2031@matsue-ct.ac.jp>
2023-11-30 16:10:14 +0900

    7 commit

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -11,6 +11,8 @@ int main() {
       cout << "Fizz, ";
    else if (i % 5 == 0)
       cout << "Buzz, ";
+   else if (i % 7 == 0)
+      cout << "git, ";
     else if (i % 15 == 0)
       cout << "FizzBuzz, ";
     else
